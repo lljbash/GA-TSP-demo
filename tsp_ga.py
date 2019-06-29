31201d6723ee0de6854053c036969aad8196a7be
Lingjie Li <lljbash@gmail.com>
2019-06-29 15:28:25 +0800

    initial commit

---
+++ tsp_ga.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tqdm
+
+
+def fitness(length):
+    return 1 / length
+
+
+def route_length(route, distance_matrix):
+    n = route.size
+    idx = np.concatenate((route, [route[0]]))
+    length = np.sum(distance_matrix[idx[:n], idx[1:n+1]])
+    # length = 0
+    # for i in range(n - 1):
+        # length += distance_matrix[route[i], route[i+1]]
+    # length += distance_matrix[route[n-1], route[0]]
+    return length
+
+
+def cx_cross(routes):
+    np.random.shuffle(routes)
+    n = routes[0].size
+    new_routes = np.zeros(n, dtype=int) - 1
+    mapped = np.zeros(n, dtype=bool)
+    i = np.random.randint(n)
+    while not mapped[routes[0][i]]:
+        new_routes[i] = routes[0][i]
+        mapped[routes[0][i]] = True
+        i = routes[1][i]
+    j = 0
+    for i in range(n):
+        if new_routes[i] == -1:
+            while mapped[routes[1][j]]:
+                j += 1
+            new_routes[i] = routes[1][j]
+            mapped[routes[1][j]] = True
+    return new_routes
+
+
+def cross(routes):
+    return cx_cross(routes)
+
+
+def mutate(route):
+    n = route.size
+    i, j = np.random.choice(n, 2, replace=False)
+    route[i], route[j] = route[j], route[i]
+
+
+def ga(distance_matrix, population_size, cross_rate, mutation_rate, max_iters, thres=-1):
+    n = distance_matrix.shape[0]
+    ncross = np.ceil(population_size * cross_rate).astype(int)
+    nreserve = population_size - ncross
+    nmutation = np.ceil(population_size * mutation_rate).astype(int)
+    best_iter = 0
+    best_route = []
+    best_fit = 0
+    for it in tqdm.trange(max_iters):
+        if it == 0:
+            population = np.array([np.random.permutation(n) for i in range(population_size)], dtype=int)
+        else:
+            idx = np.argsort(fits)
+            new_population = np.zeros(population.shape, dtype=int)
+            new_population[:nreserve, :] = population[idx[-nreserve:], :]
+            prob = fits / np.linalg.norm(fits, ord=1)
+            for i in range(ncross):
+                parents_idx = np.random.choice(population_size, 2, replace=False, p=prob)
+                new_population[nreserve+i] = cross(population[parents_idx])
+            mutation_idx = np.random.choice(population_size, nmutation, replace=False)
+            for i in mutation_idx:
+                mutate(new_population[i])
+            population = new_population
+        fits = np.array([fitness(route_length(route, distance_matrix)) for route in population])
+        for route, fit in zip(population, fits):
+            if fit > best_fit:
+                best_iter = it
+                best_route = route
+                best_fit = fit
+        if thres > 0 and it - best_iter > thres:
+            break
+    return route_length(best_route, distance_matrix), best_route, it + 1
+
+
+def load_tsplib(filename):
+    coords = []
+    with open(filename, 'r') as f:
+        node_coord_section = False
+        for line in f.readlines():
+            if line.strip() == 'EOF':
+                break
+            if node_coord_section:
+                x, y = line.strip().split()[1:3]
+                coords.append(np.array([float(x), float(y)]))
+            if line.strip() == 'NODE_COORD_SECTION':
+                node_coord_section = True
+    n = len(coords)
+    distance_matrix = np.zeros([n, n])
+    for i in range(n):
+        for j in range(n):
+            distance_matrix[i, j] = np.linalg.norm(coords[i] - coords[j])
+    return np.array(coords), distance_matrix
+
+
+if __name__ == '__main__':
+    coords, distance_matrix = load_tsplib('./xqf131.tsp')
+    length, route, it = ga(distance_matrix, population_size=128, cross_rate=0.9, mutation_rate = 0.05, max_iters=20000, thres=1000)
+    print("iteration:", it)
+    print("length:", length)
+    print("route:", list(route))
+    pts = coords[route, :]
+    plt.plot(pts[:, 0], pts[:, 1], c='k', lw=1, marker='o', mec='r', mfc='r', ms=3)
+    plt.title('iter: %d, length: %f' % (it, length))
+    plt.savefig('route.pdf')
+    plt.show()
+
